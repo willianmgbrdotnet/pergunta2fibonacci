a550f27c5384b19c58b538e63d6e8538db91cb34
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-03-06 16:58:22 -0300

    Criacao da lista Fibonacci

--- Program.cs
+++ Program.cs
@@ -12,7 +12,27 @@ class program
 
     static void Main(string[] args)
     {
-        
+        //acima de 46 ultrapassa a capacidade numerica do tipo int
+        int quantElementos = 46;
+
+        //cada numero fibonacci é formado tendo 2 valores como parametro 
+        var numeroFibo = new List<int> { 0, 1 };
+
+        //criação de numeros fibonacci e adição de cada elemento à lista
+        //de acordo com a quantidade maxima estipulada
+        while(numeroFibo.Count <= quantElementos)
+        {
+            var ultimoNumero = numeroFibo[numeroFibo.Count - 1];
+            var numeroAnterior = numeroFibo[numeroFibo.Count - 2];
+            var elemento = ultimoNumero + numeroAnterior;
+            //formação da lista
+            numeroFibo.Add(elemento);
+
+            //nome mais adequado para facilar o proximo passo.
+            var listaFibonacci = elemento;
+
+            Console.WriteLine(listaFibonacci);
+        }
     }
 
 }
